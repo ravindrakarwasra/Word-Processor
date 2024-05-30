78e32a0befa6e7bd7ca9d6d61e0c84c6c807e953
Ravindra Karwasra <154175449+ravindrakarwasra@users.noreply.github.com>
2024-05-30 13:15:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,45 @@
+let advancedOptions=document.querySelectorAll(".adv-option-button");
+const increaseTextSizeButton = document.getElementById("increaseTextSize");
+const decreaseTextSizeButton = document.getElementById("decreaseTextSize");
+const textArea = document.getElementById("textArea");
+const cutButton = document.getElementById("cutButton");
+
 function formatText(command, value=null){
     document.execCommand(command,false,value);
 }
 function undo(){
     document.execCommand('undo');
 }
-
 function redo(){
     document.execCommand('redo');
 }
+function fColor(color) {
+    document.execCommand( "foreColor", false, color );   
+}
+//main logic for colors
+const modifyText=(command,defaultUi,value)=>{
+    //elecutes command on selected text
+    document.execCommand(command,defaultUi,value);
+};
+
+
+increaseTextSizeButton.addEventListener("click", () => {
+    const currentFontSize = parseFloat(getComputedStyle(textArea).fontSize);
+    textArea.style.fontSize = (currentFontSize + 2) + "px";
+});
+  
+decreaseTextSizeButton.addEventListener("click", () => {
+    const currentFontSize = parseFloat(getComputedStyle(textArea).fontSize);
+    textArea.style.fontSize = (currentFontSize - 2) + "px";
+});
+  
+
+//for changing required parameters
+advancedOptions.forEach((button)=>{
+    button.addEventListener("change",()=>{
+        modifyText(button.id,false,button.value);
+    });
+});
 
 
 //save function
@@ -24,3 +56,28 @@ function saveAs(blob, fileName) {
     link.download = fileName;
     link.click();
 }
+
+//copy button
+
+document.getElementById("copyButton").addEventListener("click", function() {
+    var textArea = document.querySelector("#textArea");
+    var range = document.createRange();
+    range.selectNode(textArea);
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    window.getSelection().removeAllRanges();
+  });
+
+
+  //cut button
+cutButton.addEventListener("click", async () => {
+  try {
+    const selectedText = window.getSelection().toString();
+    if (selectedText) {
+      await navigator.clipboard.writeText(selectedText);
+      console.log('Content copied to clipboard');
+    }
+  } catch (err) {
+    console.error('Failed to copy: ', err);
+  }
+});
